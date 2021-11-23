9ec08a0e87b143dd3bfd1bcfe08e384e5591bffa
yuro4kaw <79467842+yuro4kaw@users.noreply.github.com>
2021-11-23 18:48:40 +0200

    Add files via upload

---
+++ Homework1/script.js
@@ -0,0 +1,63 @@
+//task 2
+console.log("Matviy");
+
+//task 3
+let kon = 5;
+let ron = 10;
+kon = ron;
+console.log(kon, ron);
+
+//task 4
+var lim = {
+  String: "hello",
+  Number: 13,
+  Boolean: true,
+  Undefined: undefined,
+  Null: null,
+};
+
+//task 5
+let isAdult = confirm("Please, confirm you are 18 or older");
+console.log(isAdult);
+
+//task 6
+let name1 = "Yuriy";
+let surname = "Matviy";
+let group = 654;
+let yearOfBirth = 2003;
+let inRelationship = true;
+let rem = 144;
+rem = null;
+let abc;
+
+console.log(group, yearOfBirth);
+console.log(inRelationship);
+console.log(name1, surname);
+console.log(rem);
+console.log(abc);
+
+//task 7
+let login = prompt("Enter your login");
+let email = prompt("Enter your e-mail");
+let password = prompt("Enter password");
+
+console.log(
+  "Dear " +
+    login +
+    ", your e-mail is " +
+    email +
+    ", your password is " +
+    password +
+    "."
+);
+
+//task 8
+let second = 1;
+let minute = second * 60;
+let hour = minute * 60;
+let day = hour * 24;
+let month = day * 30;
+
+console.log(hour);
+console.log(day);
+console.log(month);
